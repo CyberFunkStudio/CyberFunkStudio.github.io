69dd308b44de0f04539dea47b4bada215c6fdafb
Glitch (hello-webpage) <none>
2023-09-12 21:14:11 +0000

    😄🎫 Checkpoint ./play-pause.js:60511727/445

--- play-pause.js
+++ play-pause.js
@@ -1,4 +1,22 @@
+/*
 AFRAME.registerComponent('play-pause', {
+	init: function () {
+		var mySound = document.querySelector('#yellow');
+    var soundControls = document.querySelector('#soundControls');
+		this.el.addEventListener('click', function () {
+			if(mySound.paused){
+				mySound.play();
+        soundControls.setAttribute('src', '#pause');
+			} else {
+				mySound.pause();
+        soundControls.setAttribute('src', '#play');
+			}
+		});
+	}
+});
+*/
+
+AFRAME.registerComponent('play', {
 	init: function () {
 		var mySound = document.querySelector('#yellow');
     var soundControls = document.querySelector('#soundControls');
