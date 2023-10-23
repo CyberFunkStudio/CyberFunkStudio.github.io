4827a14c787596221f7543a432f6deb08918eeeb
SirenOBIC <anna.schnorf@hotmail.com>
2023-10-23 14:43:09 +0200

    center text, un-distort planes (by setting scale-factor proportionally), set Button height

--- Scripts/3d-interaction.js
+++ Scripts/3d-interaction.js
@@ -3,11 +3,11 @@ AFRAME.registerComponent('3d-interaction', {
         var el = this.el;
 
         el.addEventListener('mouseenter', function() {
-            el.setAttribute('scale','1.1 0.6 1.1')
+            el.setAttribute('scale','1.1 1.1 1.1')
         })
 
         el.addEventListener('mouseleave', function() {
-            el.setAttribute('scale','1 0.5 1')
+            el.setAttribute('scale','1 1 1')
         });
 
         el.addEventListener('click', function() {
