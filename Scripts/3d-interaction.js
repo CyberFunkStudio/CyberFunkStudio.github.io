4913a6a0761309aa9b9fe54870c34ee5941bdd33
SirenOBIC <anna.schnorf@hotmail.com>
2023-10-24 11:21:28 +0200

    manage raycast syntax and assign to buttons

--- Scripts/3d-interaction.js
+++ Scripts/3d-interaction.js
@@ -13,17 +13,13 @@ AFRAME.registerComponent('3d-interaction', {
         el.addEventListener('click', function() {
             console.log('Button clicked:', el.id);
             document.getElementById('scene').components['game-state'].setState(el.id);
+            
             if (el.id ==='Home'){
                 var aboutTextfield = document.getElementById('About-Textfield');
                 var isVisible = aboutTextfield.getAttribute('visible');
                 aboutTextfield.object3D.visible = !aboutTextfield.object3D.visible;
             }
 
-            else if (el.id ==='Contact-Textfield'){
-                var contactTextfield = document.getElementById('Contact-Textfield');
-                var isVisible = contactTextfield.getAttribute('visible');
-                contactTextfield.object3D.visible = !contactTextfield.object3D.visible;
-            }
         });
     }
 });
