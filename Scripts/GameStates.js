8178f96938c47c8241cab1152d8d10a3611a50c4
SirenOBIC <anna.schnorf@hotmail.com>
2023-10-25 10:52:13 +0200

    adding textfieldIds property to GameState class (array of IDs)

--- Scripts/GameStates.js
+++ Scripts/GameStates.js
@@ -1,9 +1,10 @@
 class GameState
 {
-    constructor(name, position, rotation) {
+    constructor(name, position, rotation, textfieldIds) {
         this.name = name;
         this.position = position;
         this.rotation = rotation;
+        this.textfieldIds = textfieldIds;
     }
 
     toString() {
@@ -12,10 +13,10 @@ class GameState
 }
 
 class GameStates {
-    static Home = new GameState('Home', {x: 0, y: 0.5, z: 6}, {x: 0, y: 0, z: 0});
-    static Team = new GameState('Team', {x: -2, y: 1, z: -1}, {x: 0, y: -70, z: 0});
-    static Contact = new GameState('Contact', {x: -1.3, y: 2.7, z: 2}, {x: 0, y: -30, z: 0});
-    static Product = new GameState('Products', {x: 0.7, y: -0.1, z: 2.3}, {x: 0, y: 20, z: 0});
+    static Home = new GameState('Home', {x: 0, y: 0.5, z: 6}, {x: 0, y: 0, z: 0}, []);
+    static Team = new GameState('Team', {x: -2, y: 1, z: -1}, {x: 0, y: -70, z: 0}, []);
+    static Contact = new GameState('Contact', {x: -1.3, y: 2.7, z: 2}, {x: 0, y: -30, z: 0}, ['Contact-Textfield', 'Mail-Textfield', 'Telephone-Textfield']);
+    static Product = new GameState('Products', {x: 0.7, y: -0.1, z: 2.3}, {x: 0, y: 20, z: 0}, []);
 
     static Matching(name) {
         var values = Object.values(GameStates);
