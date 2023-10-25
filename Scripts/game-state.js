2337594c6e467d0246ac604174815c40ecf005b6
SirenOBIC <anna.schnorf@hotmail.com>
2023-10-25 11:09:17 +0200

    fetching the array of textfields based on game state and turning them visible based on game state

--- Scripts/game-state.js
+++ Scripts/game-state.js
@@ -13,11 +13,20 @@ AFRAME.registerComponent('game-state', {
         var rot = this.state.rotation;
 
         this.setCameraPosition(pos.x, pos.y, pos.z, rot.x, rot.y, rot.z);
+        this.setTextFieldVisibility(state);
 
     },
 
     setCameraPosition: function(x, y, z, rx, ry, rz) {
         var mainCamera = document.getElementById('mainCameraWrapper').components['main-camera'];
         mainCamera.setCameraPosition(x, y, z, rx, ry, rz);
+    },
+
+    setTextFieldVisibility: function(state) {
+        var textfieldIds = GameStates.Matching(state).textfieldIds;
+        for (var i = 0; i < textfieldIds.length; i++) {
+            var textfield = document.getElementById(textfieldIds[i]);
+            textfield.setAttribute('visible', true);
+        }
     }
 });
