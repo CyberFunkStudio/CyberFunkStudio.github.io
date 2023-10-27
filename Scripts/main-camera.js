ce1dffbb825515f58e1f60b68256fb49982de831
danineuss <daniel.neuss@hey.com>
2023-10-27 10:46:34 +0200

    main-camera: reset look controls.
    This adds the TWEEN library in order to animate the look-control's yaw and pitch rotation properties since they are "traditional" three.js Object3Ds (as opposed to a-frame components) and thus lack the `.setAttribute` function.

--- Scripts/main-camera.js
+++ Scripts/main-camera.js
@@ -3,21 +3,43 @@ AFRAME.registerComponent('main-camera', {
     {
         document.getElementById('mainCamera').setAttribute('wasd-controls', 'enabled', false);
     },
+    
+    tick: function()
+    {
+        TWEEN.update();
+    },
 
-    setCameraPosition: function(x, y, z, rx, ry, rz, duration) {
+    setCameraPosition: function(x, y, z, rx, ry, rz, duration) 
+    {
         var wrapper = this.el;
+
         wrapper.setAttribute('animation__position', {
             property: 'position',
             to: {x: x, y: y, z: z},
             dur: duration,
-            easing: 'easeInOutQuad',
+            easing: 'easeInOutQuad'
         });
-
         wrapper.setAttribute('animation__rotation', {
             property: 'rotation',
             to: {x: rx, y: ry, z: rz},
             dur: duration,
-            easing: 'easeInOutQuad',
+            easing: 'easeInOutQuad'
         });
-    } 
+
+        this.resetLookControls(duration);
+    },
+
+    resetLookControls: function(duration) 
+    {
+        var lookControls = this.el.children[0].components['look-controls'];
+
+        new TWEEN.Tween(lookControls.pitchObject.rotation)
+            .to({x: 0, y: 0, z: 0}, duration)
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .start();
+        new TWEEN.Tween(lookControls.yawObject.rotation)
+            .to({x: 0, y: 0, z: 0}, duration)
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .start();
+    }
 });
