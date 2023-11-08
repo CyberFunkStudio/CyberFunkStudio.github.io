5da99b00a6e6b60cef05039a8adc2df7f1840dc6
danineuss <daniel.neuss@hey.com>
2023-11-08 10:11:21 +0100

    game-state: reset to home on second click.
    Instead of each element remembering their (possibly wrong) clicked state, the game state now resets to home if one chooses the same state twice (i.e. clicks on the same button a second time).

--- Scripts/clickable.js
+++ Scripts/clickable.js
@@ -1,12 +1,9 @@
 AFRAME.registerComponent('clickable', {
     init: function() {
         var el = this.el;
-        this.isClicked = false;
 
         el.addEventListener('click', function() {
-            document.getElementById('scene').components['game-state']
-                .setState(this.isClicked ? GameStates.Home : el.id);
-            this.isClicked = !this.isClicked;
+            document.getElementById('scene').components['game-state'].setState(el.id);
         });
     }
 });
