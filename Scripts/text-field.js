2e68f12b807516db941b5e7547d1f6b6a334bed1
CHOIX <jaha@zhaw.ch>
2024-01-11 15:22:51 +0100

    2.2 Clickable Contact-Textfields

--- Scripts/text-field.js
+++ Scripts/text-field.js
@@ -9,3 +9,24 @@ AFRAME.registerComponent('text-field', {
         }
     } 
 });
+
+
+AFRAME.registerComponent('click-listener', {
+    schema: {
+        action: { type: 'string' }
+    },
+    init: function () {
+        var action = this.data.action;
+        
+        this.el.addEventListener('click', function () {
+            console.log(action);
+        
+        if (action === 'call') {
+            window.location.href = 'tel:+41775320531';
+        }else if(action === 'email'){
+            window.location.href = 'mailto:info@cyberfunk.studio';
+        }
+        
+        });
+    }
+});
