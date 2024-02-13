c9086cc7daa029f7ba9181e3ef02a3e36ad0b30e
CHOIX <jaha@zhaw.ch>
2024-02-13 17:31:21 +0100

    2.6 transparent buttons on click

--- Scripts/clickable.js
+++ Scripts/clickable.js
@@ -3,7 +3,25 @@ AFRAME.registerComponent('clickable', {
         var el = this.el;
 
         el.addEventListener('click', function() {
+            if (el.classList.contains('products')) return;
+            const buttons = document.querySelectorAll('button'); // Select all buttons
+            buttons.forEach(button => {
+                button.classList.remove('transparent'); // Remove the "transparent" class
+            });
+            
+            const whichButton = el.getAttribute("text").value;
+            document.querySelector("."+whichButton).classList.add("transparent");
             document.getElementById('scene').components['game-state'].setState(el.id);
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButtons = document.querySelectorAll('.menu-button');
+    menuButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const state = this.getAttribute('data-state');
+        handleMenuButtonClick(state);
+      });
+    });
+  });
