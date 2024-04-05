e9b2bf9efdab9569bac186f98c4838888f3a151e
danineuss <daniel.neuss@hey.com>
2024-04-05 10:50:14 +0200

    click-listener.js: initial commit.
    Create separate component.

--- Scripts/text-field.js
+++ Scripts/text-field.js
@@ -9,21 +9,3 @@ AFRAME.registerComponent('text-field', {
         }
     } 
 });
-
-
-AFRAME.registerComponent('click-listener', {
-    schema: {
-        action: { type: 'string' }
-    },
-    init: function () {
-        var action = this.data.action;
-        
-        this.el.addEventListener('click', function () {
-            if (action === 'call') {
-                window.location.href = 'tel:+41775320531';
-            }else if(action === 'email'){
-                window.location.href = 'mailto:info@cyberfunk.studio';
-            }
-        });
-    }
-});
