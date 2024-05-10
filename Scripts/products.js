c01091ae99aedf04c1e32652f66a1d5d11002d77
CyberFunkStudio <anna@cyberfunk.studio>
2024-05-10 15:54:48 +0200

    Changing Text-Content of Products-Alerts
    Product 3 and Product 4 Button were removed. The alerts were kept in case we will re-add the buttons later.

--- Scripts/products.js
+++ Scripts/products.js
@@ -5,45 +5,36 @@ document.addEventListener('DOMContentLoaded', function() {
     function showProduct() {
         let product_text ="";
         switch (event.target.id) {
-            case "Product-1":
-                product_text =  `
-                <h1>${event.target.id}</h1>
-                <img src="http://placekitten.com/200/300" />
-                You can use <b>bold text</b>,
-                <a href="#">links</a>,
-      Sed vulputate mi sit amet mauris. Commodo elit at imperdiet dui accumsan sit amet nulla. Velit egestas dui id ornare arcu. Risus at ultrices mi tempus imperdiet. Eget magna fermentum iaculis eu non. Ut etiam sit amet nisl purus in mollis nunc. Etiam erat velit scelerisque in dictum non consectetur a. Consequat ac felis donec et odio pellentesque. Sed velit dignissim sodales ut eu sem integer vitae justo. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Tortor id aliquet lectus proin nibh nisl condimentum id. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Suspendisse sed nisi lacus sed viverra tellus in. Volutpat blandit aliquam etiam erat velit scelerisque.
+            case "Expand Web":
+                    product_text =  `
+                    <h1>All About Expand Web</h1>
+                Expand Web pages (Web blocks) can be integrated into existing websites or stand alone as independent websites (Home Planets). Unlike traditional flat web pages, our Web products are spatial and present information in an interactive three-dimensional space. Cyber Funk Studio handles the conception, design, and technical integration of these digital spaces.
+                Through the design of Expand Web products, complex data can be visually represented, making it more tangible and comprehensible. Another strength of the Expand Web products is the ability to convey a narrative with great depth. This is suitable for clients who want to depict complex processes or for projects that aim to tell an emotional story.
+                
               `
                 break;
-            case "Product-2":
+            case "Expand Print":
                     product_text =  `
-                    <h1>${event.target.id}</h1>
-                    You can use <b>bold text</b>,
-                    <a href="#">links</a>,
-                <img src="http://placekitten.com/200/301" />
-
-          Dolor purus non enim praesent. Lectus sit amet est placerat in egestas. Pellentesque dignissim enim sit amet venenatis. Eget magna fermentum iaculis eu non diam phasellus. Massa enim nec dui nunc mattis enim ut tellus elementum. At risus viverra adipiscing at. Interdum velit laoreet id donec ultrices. Orci phasellus egestas tellus rutrum tellus pellentesque. Vitae sapien pellentesque habitant morbi tristique. Lorem ipsum dolor sit amet. Ipsum dolor sit amet consectetur adipiscing elit. Nunc id cursus metus aliquam eleifend mi in. In hac habitasse platea dictumst quisque sagittis purus sit amet. Malesuada pellentesque elit eget gravida cum.
+                    <h1>All About Expand Print</h1>
+                Digital content is made accessible via Web-AR technology on a print medium, eliminating the need for downloading an app. The virtual content complements the analog information, creating an exciting collage between analog qualities and digital possibilities. Business cards, brochures, and books become physical anchors and provide a projection surface for three-dimensional models or videos. Content can be swapped out multiple times, dynamically telling a story over a certain duration. Thus, our Expand Print products not only represent information in three dimensions but, with the ability to tell stories over time, they even acquire a fourth dimension.
                   `
                     break;
             case "Product-3":
                         product_text =  `
-                        <h1>${event.target.id}</h1>
-                        You can use <b>bold text</b>,
-                <img src="http://placekitten.com/200/303" />
-
-                        <a href="#">links</a>,
-                Dolor purus non enim praesent. Lectus sit amet est placerat in egestas. Pellentesque dignissim enim sit amet venenatis. Eget magna fermentum iaculis eu non diam phasellus. Massa enim nec dui nunc mattis enim ut tellus elementum. At risus viverra adipiscing at. Interdum velit laoreet id donec ultrices. Orci phasellus egestas tellus rutrum tellus pellentesque. Vitae sapien pellentesque habitant morbi tristique. Lorem ipsum dolor sit amet. Ipsum dolor sit amet consectetur adipiscing elit. Nunc id cursus metus aliquam eleifend mi in. In hac habitasse platea dictumst quisque sagittis purus sit amet. Malesuada pellentesque elit eget gravida cum.
+                        <h1>Comming soon</h1>
                         `
                     break;
             case "Product-4":
                             product_text =  `
-                <img src="http://placekitten.com/200/305" />
-
-                            <h1>${event.target.id}</h1>
-                            You can use <b>bold text</b>,
-                            <a href="#">links</a>,
-                    Leo urna molestie at elementum eu. Facilisis volutpat est velit egestas dui id. Facilisi etiam dignissim diam quis enim lobortis. Cras ornare arcu dui vivamus arcu felis bibendum ut. Natoque penatibus et magnis dis parturient montes nascetur ridiculus. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Amet purus gravida quis blandit. Semper quis lectus nulla at. Purus sit amet volutpat consequat. Dignissim cras tincidunt lobortis feugiat vivamus at augue. Et molestie ac feugiat sed. Fermentum dui faucibus in ornare quam viverra. Aliquam faucibus purus in massa tempor nec feugiat. A cras semper auctor neque vitae tempus quam. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Feugiat pretium nibh ipsum consequat nisl.
+                            <h1>Comming Soon</h1>
                             `
-                    break;
+                    break;    
+            case "About-button":
+                      product_text =  `
+                      <h1>About Cyber Funk Studio</h1>
+                      Cyber Funk Studio strives to bridge the gap between Extended Reality and traditional communication media. We make digital content tangible in physical space by merging the analog with the digital. This creates powerful, unique hybrids that offer a glimpse into the future.
+                      `
+                    break;     
 
             default:
                 break;
